9cd55b6e1190e7b7b4cda5f3b175fbd24f4145e5
ekuri emily <ekuri emily>
2016-04-15 19:42:31 +0800

    create filesystem tree when start up

---
+++ public/javascripts/filesystem.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+fs.mkdir("./data", function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        fs.mkdir("./data/uploaded/", function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                fs.mkdir("./data/uploaded/finished/", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+
+                fs.mkdir("./data/uploaded/tmp/", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
+    }
+});
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 
 // ManLvTu modules
+// initialize filesystem
+require('./public/javascripts/filesystem.js');
 // initialize database
 require('./public/javascripts/database.js');
 // initialize routers
 var topRouter = require('./routes/topRouter.js');
 app.use('/', topRouter);
 
-var server = app.listen(33000, '121.42.216.167', function() {
+var server = app.listen(33000, '172.19.54.42', function() {
     console.log('server listening on %s %s', server.address().address, server.address().port);
 });
 
